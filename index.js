3343c93586afb0aa1feebaca3a5afe110f495ec5
Dafrok <o.o@mug.dog>
2020-08-04 20:25:51 +0800

    feature: 开启膜拜

--- index.js
+++ index.js
@@ -1,119 +1,113 @@
+/**
+ * @file entry
+ * @author o.o@mug.dog
+ */
+
+const Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keynZdgF8AWA4v8OA'
+});
+const base = Airtable.base('app4cVgEKayWtPKz8');
+
 function renderComments(list) {
-    var $list = document.getElementById('comment-list');
-    var fragment = new DocumentFragment();
+    const fragment = new DocumentFragment();
     list.forEach(item => {
-        var $comment = document.createElement('div');
-        var $name = document.createElement('div');
-        var $content = document.createElement('div');
-        $name.innerText = item.name || '匿名';
-        $content.innerText = item.content;
+        const $comment = document.createElement('div');
+        const $username = document.createElement('div');
+        const $content = document.createElement('div');
+        const $datetime = document.createElement('span');
+
+        $username.innerText = item.username || '匿名';
+        $content.innerText = item.content || '';
+
+        const d = new Date(item.datetime);
+        $datetime.innerText = new Date(item.datetime) <= 1596530702000
+            ? ' - 很久以前'
+            : ` - ${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 
         $comment.classList.add('box');
-        $name.classList.add('title', 'is-5')
+        $username.classList.add('has-text-weight-bold', 'is-size-4');
+        $datetime.classList.add('has-text-grey-dark', 'has-text-weight-light', 'is-size-7', 'ml-2');
 
-        $comment.appendChild($name);
+        $comment.appendChild($username);
+        $username.appendChild($datetime);
         $comment.appendChild($content);
         fragment.appendChild($comment);
     });
+    return fragment;
+}
+
+function insertComments(list) {
+    const fragment = renderComments(list);
+    const $list = document.getElementById('comment-list');
+    $list.insertBefore(fragment, $list.childNodes[0]);
+}
+
+function appendComments(list) {
+    const fragment = renderComments(list);
+    const $list = document.getElementById('comment-list');
     $list.appendChild(fragment);
 }
 
 function getList() {
-    fetch('godqian.json')
-        .then(res => res.json())
-        .then(renderComments);
+    base('Godqian').select({
+        sort: [{field: 'datetime', direction: 'desc'}],
+        pageSize: 50,
+        view: 'Worship'
+    }).eachPage(function page(records, fetchNextPage) {
+        appendComments(
+            records.map(record => ({
+                username: record.get('username'),
+                content: record.get('content'),
+                datetime: record.get('datetime')
+            })
+        ));
+        window.nextPageHandler = fetchNextPage;
+    }, function done(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
 }
 
+const $comment = document.getElementById('comment');
+$comment.addEventListener('submit', e => {
+    e.preventDefault();
+    const $formItems = e.target.elements;
+    const fields = {};
+    for (const $item of $formItems) {
+        if ($item.name) {
+            fields[$item.name] = $item.value;
+        }
+    }
+    base('Godqian').create([
+        {fields}
+    ], (err, records) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        e.target.reset();
+        insertComments(
+            records.map(record => ({
+                username: record.get('username'),
+                content: record.get('content'),
+                datetime: record.get('datetime')
+            })
+        ));
+    });
+});
+
+function nextPage(e) {
+    const maxHeight = document.body.clientHeight;
+    const scrollTop = document.scrollTop || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    if (window.nextPageHandler && windowHeight + scrollTop >= maxHeight) {
+        window.nextPageHandler();
+    }
+}
+
+window.onscroll = nextPage;
+
 window.onload = getList;
-
-// function isIE(){
-//     //ie? 
-//     if (window.navigator.userAgent.indexOf("MSIE")>=1) 
-//         return true; 
-//     else 
-//         return false; 
-// } 
-
-// if(!isIE()){ //firefox innerText define
-//     HTMLElement.prototype.__defineGetter__(    "innerText", 
-//         function(){ 
-//             return this.textContent; 
-//         } 
-//     ); 
-//     HTMLElement.prototype.__defineSetter__(    "innerText", 
-//         function(sText){ 
-//             this.textContent=sText; 
-//         } 
-//     ); 
-// }
-// function gel(id)
-// {
-//     return document.getElementById(id);
-// }
-// function gelt(e)
-// {
-//     if(e.textContent)
-//         return(e.textContent);
-//     else if(e.text)
-//         return(e.text);
-//     else
-//         return "N/A";
-// }
-// function xmlHttp()
-// {
-//     if(window.ActiveXObject)
-//     {
-//         return new ActiveXObject("Microsoft.XMLHTTP");
-//     }
-//     else if(window.XMLHttpRequest)
-//     {
-//         return new window.XMLHttpRequest()
-//     }
-//     else
-//     {
-//         return null;
-//     }
-// }
-// function getList()
-// {
-//     var xhr=xmlHttp();
-//     if(xhr==null)
-//     {
-//         gel("list").text="浏览器不支持Ajax，无法显示。";
-//         return;
-//     }
-//     xhr.open("GET","godqian.xml?"+Math.random());
-//     xhr.onreadystatechange=function()
-//     {
-//         if(xhr.readyState==4)
-//         {
-//             gel("list").innerHTML="";
-//             for(i=0;i<xhr.responseXML.getElementsByTagName("entry").length;i++)
-//             {
-//                 gel("list").innerHTML+="<a onclick=\"gel('tip').innerText='"+gelt(xhr.responseXML.getElementsByTagName("entry")[i]).replace(/"/g,"&quot;")+"';gel('tip').innerHTML=gel('tip').innerHTML.replace(/\\[imp\\]/ig,'<span class=\\'important\\'>').replace(/\\[\\/imp\\]/ig,'</span>');\" onmouseover=\"return true;\" href=\"#atip\">·"+xhr.responseXML.getElementsByTagName("entry")[i].getAttribute("name")+"</a><br />";
-//             }
-//         }
-//     }
-//     xhr.send(null);
-// }
-// function submitTip()
-// {
-//     var query="";
-//     query="Name="+gel("txtName").value+"&Tip="+gel("txtTip").value.replace(/\\/g,"\\\\").replace(/\n/g,"\\r\\n").replace(/'/g,"\\'");
-//     var xhr=xmlHttp();
-//     if(xhr==null)
-//     {
-//         alert("浏览器不支持Ajax，无法提交。");
-//         return;
-//     }
-//     xhr.open("POST","submittips.asp");
-//     xhr.onreadystatechange=function()
-//     {
-//         if(xhr.readystate==4)
-//         {
-//             getList();
-//         }
-//     }
-//     xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-//     xhr.send(query);
-// }
